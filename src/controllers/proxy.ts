814fbd3c7dd919438f766f895a85046b5ec68015
Brandon Wilson <brandon@coil.com>
2019-09-17 23:42:28 -0500

    Proxy to container

--- src/controllers/proxy.ts
+++ src/controllers/proxy.ts
@@ -1,47 +1,120 @@
 import * as Hapi from '@hapi/hapi'
 import * as Boom from 'boom'
+import * as React from 'react'
+import { renderToString } from 'react-dom/server'
 import { Injector } from 'reduct'
+import ProxyComponent from '../public/components/Proxy'
 import Config from '../services/Config'
 
-const HttpProxy = require('http-proxy')
-const PROXY_LABEL_REGEX = /^proxy-[a-zA-Z2-7]{52}$/
+const Proxy = React.createFactory(ProxyComponent)
+const PROXY_SUBDOMAIN_REGEX = /^proxy-[a-zA-Z2-7]{52}$/
+const MANIFEST_SUBDOMAIN_REGEX = /^[a-zA-Z2-7]{52}$/
+
+const proxyPath = '/proxy'
+const proxyPagePath = '/proxypage'
 
 import { create as createLogger } from '../common/log'
 const log = createLogger('proxy')
 
+// Containers are accessed from the browser at:
+//
+//   https://hash.host-domain/path
+//
+// onRequest modifies the path to be handled by the proxy page route:
+//
+//   https://hash.host-domain/proxypage/path
+//
+// The web-monetized proxy page accesses the container at:
+//
+//   https://proxy-hash.hostdomain/path
+//
+// onRequest modifies the path to be handled by the proxy route:
+//
+//   http://proxy-hash.host-domain/proxy/path
+//
+// The server proxies the request to k8s/knative at:
+//
+//   http://proxy-hash.namespace.svc.cluster.local/path
+
 export default function (server: Hapi.Server, deps: Injector) {
   const config = deps(Config)
-  const proxy = HttpProxy.createProxyServer()
 
   function isProxyRequest (host: string): boolean {
-    return !!PROXY_LABEL_REGEX.exec(host.split('.')[0])
+    return !!PROXY_SUBDOMAIN_REGEX.exec(host.split('.')[0])
   }
 
-  async function proxyToPod (request: Hapi.Request, h: any) {
+  function isProxyPageRequest (host: string): boolean {
+    return !!MANIFEST_SUBDOMAIN_REGEX.exec(host.split('.')[0])
+  }
+
+  async function onRequest (request: Hapi.Request, h: Hapi.ResponseToolkit) {
     const host = request.info.host
 
-    if (!isProxyRequest(host)) {
-      return h.continue
+    if (isProxyRequest(host)) {
+      request.setUrl(`http://${request.info.host}${proxyPath}${request.path}`)
+    } else if (isProxyPageRequest(host)) {
+      request.setUrl(`http://${request.info.host}${proxyPagePath}${request.path}`)
     }
 
-    // TODO: https
-    const target = `http://${host.split('.')[0]}.${config.k8sNamespace}.svc.cluster.local`
-    console.log(target)
-    await new Promise((resolve, reject) => {
-      proxy.web(request.raw.req, request.raw.res, { target }, (e: any) => {
-        const statusError = {
-          ECONNREFUSED: Boom.serverUnavailable(),
-          ETIMEOUT: Boom.gatewayTimeout()
-        }[e.code]
+    return h.continue
+  }
 
-        if (statusError) {
-          reject(statusError)
-        }
+  async function getProxyPage (request: Hapi.Request, h: Hapi.ResponseToolkit) {
+    const host = request.info.host
 
-        resolve()
-      })
+    if (!isProxyPageRequest(host)) {
+      throw Boom.notFound()
+    }
+
+    const state = {
+      // TODO: https
+      proxySrc: `http://proxy-${request.info.host}/${request.params.path}`
+    }
+
+    return h.view('index', {
+      // Use publicUri instead of current subdomain so onRequest doesn't change the path
+      assets: `${config.publicUri}/assets/proxy.bundle.js`,
+      component: renderToString(Proxy(state)),
+      paymentPointer: config.paymentPointer,
+      state: `window.__INITIAL_PROPS__ = ${JSON.stringify(state)}`
     })
   }
 
-  server.ext('onRequest', proxyToPod)
+  async function proxy (request: Hapi.Request, h: Hapi.ResponseToolkit) {
+    const host = request.info.host
+
+    if (!isProxyRequest(host)) {
+      throw Boom.notFound()
+    }
+
+    try {
+      return h.proxy({
+        // TODO: https
+        uri: `http://${host.split('.')[0]}.${config.k8sNamespace}.svc.cluster.local/${request.params.path}`,
+        passThrough: true
+      })
+    } catch (e) {
+      log.error(e)
+      Boom.serverUnavailable()
+    }
+  }
+
+  server.ext('onRequest', onRequest)
+
+  server.route({
+    method: 'GET',
+    path: `${proxyPagePath}/{path*}`,
+    handler: getProxyPage
+  })
+
+  server.route({
+    method: '*',
+    path: `${proxyPath}/{path*}`,
+    handler: proxy,
+    options: {
+      payload: {
+        parse: false
+      }
+    }
+  })
 }
--- src/controllers/static.ts
+++ src/controllers/static.ts
@@ -3,22 +3,15 @@ import * as React from 'react'
 import { renderToString } from 'react-dom/server'
 import { Injector } from 'reduct'
 import AppComponent from '../public/components/App'
-import ProxyComponent from '../public/components/Proxy'
 import Config from '../services/Config'
 import * as path from 'path'
 
 const App = React.createFactory(AppComponent)
-const Proxy = React.createFactory(ProxyComponent)
-const MANIFEST_LABEL_REGEX = /^[a-zA-Z2-7]{52}$/
 
 export default function (server: Hapi.Server, deps: Injector) {
   const config = deps(Config)
 
-  function isPodRequest (host: string): boolean {
-    return !!MANIFEST_LABEL_REGEX.exec(host.split('.')[0])
-  }
-
-  async function getApp (request: Hapi.Request, h: Hapi.ResponseToolkit) {
+  async function getIndex (request: Hapi.Request, h: Hapi.ResponseToolkit) {
     const state = {
       // TODO: set min version in config
       // codiusVersion: config.version.codius.min
@@ -33,32 +26,9 @@ export default function (server: Hapi.Server, deps: Injector) {
     })
   }
 
-  async function getProxy (request: Hapi.Request, h: Hapi.ResponseToolkit) {
-    const state = {
-      // TODO: https
-      proxySrc: `http://proxy-${request.info.host}${request.path}`
-    }
-
-    return h.view('index', {
-      assets: '/assets/proxy.bundle.js',
-      component: renderToString(Proxy(state)),
-      paymentPointer: config.paymentPointer,
-      state: `window.__INITIAL_PROPS__ = ${JSON.stringify(state)}`
-    })
-  }
-
-  async function getIndex (request: Hapi.Request, h: Hapi.ResponseToolkit) {
-    const host = request.info.host
-
-    const proxy = isPodRequest(host)
-    const state = {}
-
-    return isPodRequest(request.info.host) ? getProxy(request, h) : getApp(request, h)
-  }
-
   server.route({
     method: 'GET',
-    path: '/{params*}',
+    path: '/',
     handler: getIndex
   })
 }
